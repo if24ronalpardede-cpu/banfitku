df29671df945afa05ff4d8056418a0ef14375bc5
xbagasss <bagaswarda005@gmail.com>
2026-04-23 16:49:59 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'BanFitKu',
+  webDir: 'www'
+};
+
+export default config;
